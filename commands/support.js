a19b6ee96189c565e26b22c8b80ddb9278feb6e6
Mark Townsend <mark.townsend@semcon.com>
2021-12-16 23:08:33 +0000

    Moved createsupport functionality into the support command instead.

--- commands/support.js
+++ commands/support.js
@@ -4,22 +4,54 @@ module.exports = {
 	execute(message, args) {
         const supportRole = message.guild.roles.cache.find(role => role.name === 'Support Staff');
         
-        // If the Support Staff role doesn't exist on this server, just stop
-		if(!supportRole) {console.log("Support command not active on this server"); return;};
+        
+
+        function activateSupport(msg){
+            // Check to make sure the current user is an admin user. Only admin users can use this command
+            if (!message.member.permissions.has("ADMINISTRATOR")) {
+                message.channel.send(`What are you doing?! Only administrators can use that command!`)
+                                .then(msg => { 
+                                    setTimeout(() => {msg.delete()}, 10000);
+                                });
+                return;
+            }
+            
+            // Check to make sure the support role doesn't already exist
+            if (!message.guild.roles.cache.find(role => role.name === "Support Staff")) {
+                message.guild.roles.create({
+                    name: 'Support Staff',
+                    color: '#db2edb',
+                    mentionable: true,
+                    reason: "Support staff are needed to help our guests"
+                });
+            };
+        }
+
+        function isActive(msg) {
+            // If the Support Staff role doesn't exist on this server, just stop
+            console.log("Support command is not activated on this server");
+            console.log(!(!supportRole));
+		    return !(!supportRole); 
+        }
 
         function joinSupport(msg) {
+            if(!isActive(msg)) return;
             msg.member.roles.add(supportRole);
         }
 
         function leaveSupport(msg) {
+            if(!isActive(msg)) return;
             msg.member.roles.remove(supportRole);
         }
 
         function help(msg) {
-            msg.channel.send("Use `$support join` to join the Support Staff, or `$support leave` in order to leave the Support Staff.").catch(console.error);
+            if(!isActive(msg)) return;
+            msg.channel.send("Use `$support join` to join the Support Staff, or `$support leave` in order to leave the Support Staff. To deactivate the support system, simply delete the Support Staff role.").catch(console.error);
         }
         
         switch (args[0]) {
+            case "activate":
+            activateSupport(message);
             case "join":
             joinSupport(message);
             break;
