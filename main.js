a99f5b888a100598cd445f49a3c955f68ad8f6e9
whackablemole <63235752+whackablemole@users.noreply.github.com>
2021-12-17 09:23:39 +0000

    Added BOT_PREFIX to environment variables.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"] });
 
-const prefix = "$";
+const prefix = process.env.BOT_PREFIX;
 
 const fs = require('fs');
 
